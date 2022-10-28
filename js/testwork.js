c22c354f70b3cbc9372443964681a94a68b7c05c
Miakishev <makisevm12@gmail.com>
2022-10-28 23:26:56 +0300

    цвфцц

---
+++ js/testwork.js
@@ -0,0 +1,10 @@
+let balance = 10000;
+const payment = 2000;
+
+console.log(
+  "Общая стоимость заказа ${2000} кредитов.Проверим кол-во доступніх средств на счету"
+);
+
+if (balance >= payment) {
+  console.log("ok");
+}
